52920be88c09edcee12b3a9b05d3f51d7359a22b
DesistDaydream <373406000@qq.com>
2022-06-02 14:36:13 +0800

    2022.6.2

--- cmd/kvm.go
+++ cmd/kvm.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"time"
 
 	"github.com/sirupsen/logrus"
 	"libvirt.org/go/libvirt"
+
+	humanize "github.com/dustin/go-humanize"
 )
 
 func main() {
@@ -19,13 +21,27 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%d running domains:\n", len(doms))
+	logrus.Info("%d running domains:\n", len(doms))
+
 	for _, dom := range doms {
 		d, err := dom.GetInfo()
-		if err == nil {
-			logrus.Error(d)
+		if err != nil {
+			logrus.Error("获取虚拟机信息异常: ", err)
 		}
-		logrus.Info(d)
+
+		name, err := dom.GetName()
+		if err != nil {
+			logrus.Error("获取虚拟机名称异常: ", err)
+		}
+
+		logrus.WithFields(logrus.Fields{
+			"状态":      d.State,
+			"最大内存":    humanize.IBytes(d.MaxMem),
+			"已用内存":    humanize.IBytes(d.Memory),
+			"逻辑CPU数":  d.NrVirtCpu,
+			"CPU运行时间": time.Nanosecond * time.Duration(d.CpuTime),
+		}).Infof("%v 虚拟机信息", name)
+
 		dom.Free()
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/DesistDaydream/go-libvirt
 go 1.17
 
 require (
+	github.com/dustin/go-humanize v1.0.0
 	github.com/sirupsen/logrus v1.8.1
 	libvirt.org/go/libvirt v1.8001.0
 )
