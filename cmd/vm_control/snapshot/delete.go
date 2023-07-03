6f49afcb989cc454a3341727dd98eacffe0b38b2
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2023-07-03 17:19:22 +0800

    update

--- cmd/vm_control/snapshot/delete.go
+++ cmd/vm_control/snapshot/delete.go
@@ -40,3 +40,36 @@ func runDelete(cmd *cobra.Command, args []string) {
 		logrus.Infof("已成功删除 %v 中的 %v 快照", n.domainName, snapshotFlags.SnapshotName)
 	}
 }
+
+// 不先获取需要处理的虚拟机，而是找到一个处理一个
+// TODO: 这种方式和先找到再处理哪个合适呢
+// func runDelete(cmd *cobra.Command, args []string) {
+// 	if snapshotFlags.SnapshotName == "" || snapshotFlags.DomainName == nil {
+// 		logrus.Fatal("请指定快照名称和虚拟机名称")
+// 	}
+// 	for _, conn := range handler.Conns {
+// 		hostname, _ := conn.GetHostname()
+// 		for dNameIndex, dName := range snapshotFlags.DomainName {
+// 			domain, _ := conn.LookupDomainByName(dName)
+// 			if domain != nil {
+// 				logrus.Infof("在 %v 上找到虚拟机 %v", hostname, dName)
+// 				snapshot, err := domain.SnapshotLookupByName(snapshotFlags.SnapshotName, 0)
+// 				if err != nil {
+// 					logrus.Errorf("无法在【%v】中找到【%v】快照，原因: %v", dName, snapshotFlags.SnapshotName, err)
+// 					continue
+// 				}
+// 				defer snapshot.Free()
+// 				err = snapshot.Delete(libvirt.DOMAIN_SNAPSHOT_DELETE_CHILDREN)
+// 				if err != nil {
+// 					logrus.Errorf("删除快照失败，原因: %v", err)
+// 					continue
+// 				} else {
+// 					logrus.Infof("已成功删除 %v 中的 %v 快照", dName, snapshotFlags.SnapshotName)
+// 					// TODO: 删除 snapshotFlags.DomainName 中的 dName 元素
+// 					snapshotFlags.DomainName = append(snapshotFlags.DomainName[:dNameIndex], snapshotFlags.DomainName[dNameIndex+1:]...)
+// 				}
+// 				domain.Free()
+// 			}
+// 		}
+// 	}
+// }
